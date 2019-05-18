dd1258a454481191c78589d5208f1a5f2ee2e796
PSNRigner <loic.frasse-mathon@epitech.eu>
2019-05-18 12:06:44 +0200

    Updated plugin messages to 1.13+

--- src/main/java/net/badlion/timers/TimerPlugin.java
+++ src/main/java/net/badlion/timers/TimerPlugin.java
@@ -6,23 +6,16 @@ import net.badlion.timers.listeners.TimerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.nio.charset.Charset;
-import java.util.logging.Level;
 
 public class TimerPlugin extends JavaPlugin {
 
-	public static final String CHANNEL_NAME = "BLC|T";
+	public static final String CHANNEL_NAME = "badlion:timers";
 	public static final Charset UTF_8_CHARSET = Charset.forName("UTF-8"); // Do not use Guava because of 1.7
 
 	private TimerApiImpl timerApi;
 
 	@Override
 	public void onEnable() {
-		// Only support <= 1.12.2 at the moment, we will add 1.13 support when BLC 1.13 is ready
-		if (this.getServer().getBukkitVersion().startsWith("1.13")) {
-			this.getLogger().log(Level.SEVERE, "BLC Timer API is not currently compatible with 1.13 Minecraft. Check back later for updates.");
-			this.getPluginLoader().disablePlugin(this);
-			return;
-		}
 
 		NmsManager.init(this);
 
